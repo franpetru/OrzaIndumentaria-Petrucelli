fdd04670f4e1329094d6dd45533e4e9d045c4555
franpetru <80796659+franpetru@users.noreply.github.com>
2021-07-25 21:09:04 -0300

    Add files via upload

---
+++ app/src/components/NavBar.js
@@ -0,0 +1,14 @@
+
+const NavBar = () => 
+    <>
+    
+        <nav className="navBar">
+            <h1>Orza Indumentaria</h1>
+            <p><a href="#">Inicio</a></p>
+            <p><a href="#">Catalogo</a></p>
+            <p><a href="#">Contacto</a></p>
+            <p><a href="#">Tienda</a></p>
+        </nav>
+    </>
+
+export default NavBar 
